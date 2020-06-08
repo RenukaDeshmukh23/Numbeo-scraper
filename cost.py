fe759cd8d81b1bcfcb36275b134cad8d44c589e8
Renuka Deshmukh <53084819+RenukaDeshmukh23@users.noreply.github.com>
2020-06-08 12:39:13 +0530

    Add files via upload

--- cost.py
+++ cost.py
@@ -7,14 +7,14 @@ country=pyperclip.paste()
 class CostSpider(scrapy.Spider):
     name = 'cost'
     allowed_domains = ['numbeo.com/cost-of-living/']
-    start_urls = ['https://www.numbeo.com/cost-of-living/country_result.jsp?country='+country+'&displayCurrency=USD']
+    start_urls = ['https://www.numbeo.com/cost-of-living/in/'+country+'?displayCurrency=USD']
 
     def parse(self, response):
-        for table in response.xpath('//*[@class="data_wide_table"]//tr'):
+        for table in response.xpath('//*[@class="data_wide_table new_bar_table"]//tr'):
             head=table.xpath('.//th[1]/text()').extract_first()
             name= table.xpath('.//td[1]/text()').extract_first()
             price= table.xpath('.//td[2]//text()').extract_first()
-            range=table.xpath('.//td[3]//text()').extract()
+            range=table.xpath('.//td[3]//span//text()').extract()
 
             yield {'Heading':head,
                     'Name':name,
